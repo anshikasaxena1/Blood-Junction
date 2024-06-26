f40ef21395b97ddafff038b881a3fe6a3d80ae6a
Anshika Saxena <anshiksaxena310@gmail.com>
2024-06-26 18:58:31 +0530

    new files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Body from "./components/Body";
+
 
 function App() {
   return (
-    <div className="App">
-          hello me
+    <div >
+          
+          <Body/>
        
     </div>
   );
---
+++ src/components/Body.js
@@ -0,0 +1,38 @@
+
+import React from 'react'
+import Landing from './Landing'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Login from './Login'
+import Signup from './Signup'
+import Home from './Home'
+
+const Body = () => {
+
+    const appRouter=createBrowserRouter([
+        {
+            path:"/",
+            element: <Landing/>
+        },
+        {
+            path:"/login",
+            element: <Login/>
+        },
+        {
+            path:"signup",
+            element: <Signup/>
+        },
+        {
+            path:"home",
+            element:<Home/>
+        }
+    ])
+
+
+  return (
+    <div>
+        <RouterProvider router={appRouter}/>
+    </div>
+  )
+}
+
+export default Body
